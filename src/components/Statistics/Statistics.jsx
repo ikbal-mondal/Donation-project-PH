74d70c30b09a6301612386e8a96e9633d218819b
ikbal <ikbalmondal062@gmail.com>
2023-09-27 14:14:07 +0530

    added 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,47 @@
 
+import { PieChart, Pie,  Cell, ResponsiveContainer } from 'recharts';
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+
+];
+
+const COLORS = ['#FF444A', '#00C49F', ];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 const Statistics = () => {
   return (
-    <div>
-      <h1 className="text-3xl bg-red-400">this is a chart component </h1>
-    </div>
+    <ResponsiveContainer width={600} height={500}>
+    <PieChart  width={600} height={500}>
+      <Pie
+        data={data}
+        cx="80%"
+        cy="50%"
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+    </PieChart>
+  </ResponsiveContainer>
   );
 };
 
